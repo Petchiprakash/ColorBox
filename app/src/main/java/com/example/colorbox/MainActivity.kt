5d9bcf2a4554343fcc48aa1c5f95a1346f1aaf59
Petchiprakash <petchiprakash2@gmail.com>
2021-11-23 09:58:52 +0530

    Used Scope Functions

--- app/src/main/java/com/example/colorbox/MainActivity.kt
+++ app/src/main/java/com/example/colorbox/MainActivity.kt
@@ -10,70 +10,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnRed.setOnClickListener {
-            binding.imgBox1.setOnClickListener {
-                binding.imgBox1.setImageResource(R.drawable.btn_shp)
-
-            }
-            binding.imgBox2.setOnClickListener {
-                binding.imgBox2.setImageResource(R.drawable.btn_shp)
-
-            }
-            binding.imgBox3.setOnClickListener {
-                binding.imgBox3.setImageResource(R.drawable.btn_shp)
-
-            }
-            binding.imgBox4.setOnClickListener {
-                binding.imgBox4.setImageResource(R.drawable.btn_shp)
-
-            }
-            binding.imgBox5.setOnClickListener {
-                binding.imgBox5.setImageResource(R.drawable.btn_shp)
-
+        binding.apply {
+            btnRed.setOnClickListener {
+                imgBox1.setOnClickListener {
+                    imgBox1.setImageResource(R.drawable.btn_shp)
+                }
+                imgBox2.setOnClickListener {
+                    imgBox2.setImageResource(R.drawable.btn_shp)
+                }
+                imgBox3.setOnClickListener {
+                    imgBox3.setImageResource(R.drawable.btn_shp)
+                }
+                imgBox4.setOnClickListener {
+                    imgBox4.setImageResource(R.drawable.btn_shp)
+                }
+                imgBox5.setOnClickListener {
+                    imgBox5.setImageResource(R.drawable.btn_shp)
+                }
             }
         }
-        binding.btnYellow.setOnClickListener {
-            binding.imgBox1.setOnClickListener {
-                binding.imgBox1.setImageResource(R.drawable.btn_shp2)
-
-            }
-            binding.imgBox2.setOnClickListener {
-                binding.imgBox2.setImageResource(R.drawable.btn_shp2)
-
-            }
-            binding.imgBox3.setOnClickListener {
-                binding.imgBox3.setImageResource(R.drawable.btn_shp2)
-
-            }
-            binding.imgBox4.setOnClickListener {
-                binding.imgBox4.setImageResource(R.drawable.btn_shp2)
-
-            }
-            binding.imgBox5.setOnClickListener {
-                binding.imgBox5.setImageResource(R.drawable.btn_shp2)
-
+        binding.apply {
+            btnYellow.setOnClickListener {
+                imgBox1.setOnClickListener {
+                    imgBox1.setImageResource(R.drawable.btn_shp2)
+                }
+                imgBox2.setOnClickListener {
+                    imgBox2.setImageResource(R.drawable.btn_shp2)
+                }
+                imgBox3.setOnClickListener {
+                    imgBox3.setImageResource(R.drawable.btn_shp2)
+                }
+                imgBox4.setOnClickListener {
+                    imgBox4.setImageResource(R.drawable.btn_shp2)
+                }
+                imgBox5.setOnClickListener {
+                    imgBox5.setImageResource(R.drawable.btn_shp2)
+                }
             }
         }
-        binding.btnGreen.setOnClickListener {
-            binding.imgBox1.setOnClickListener {
-                binding.imgBox1.setImageResource(R.drawable.btn_shp3)
-
-            }
-            binding.imgBox2.setOnClickListener {
-                binding.imgBox2.setImageResource(R.drawable.btn_shp3)
-
-            }
-            binding.imgBox3.setOnClickListener {
-                binding.imgBox3.setImageResource(R.drawable.btn_shp3)
-
-            }
-            binding.imgBox4.setOnClickListener {
-                binding.imgBox4.setImageResource(R.drawable.btn_shp3)
-
-            }
-            binding.imgBox5.setOnClickListener {
-                binding.imgBox5.setImageResource(R.drawable.btn_shp3)
-
+        binding.apply {
+            btnGreen.setOnClickListener {
+                imgBox1.setOnClickListener {
+                    imgBox1.setImageResource(R.drawable.btn_shp3)
+                }
+                imgBox2.setOnClickListener {
+                    imgBox2.setImageResource(R.drawable.btn_shp3)
+                }
+                imgBox3.setOnClickListener {
+                    imgBox3.setImageResource(R.drawable.btn_shp3)
+                }
+                imgBox4.setOnClickListener {
+                    imgBox4.setImageResource(R.drawable.btn_shp3)
+                }
+                imgBox5.setOnClickListener {
+                    imgBox5.setImageResource(R.drawable.btn_shp3)
+                }
             }
         }
     }
